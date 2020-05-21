956b7e1f01fe798fbdcfcec6f808b23f13548bed
thaibinhnguyen <thaibinhnguyen26041996@gmail.com>
2020-05-21 18:22:39 +0200

    implement data api into cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
+//components
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import { Cards, Chart, CountryPicker } from './components/index';
+import { fetchData } from './api'
+
+//style
+import styles from './App.module.css';
+
 class App extends React.Component {
+    state = {
+        data: {}
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        
+        this.setState({ data: fetchedData})
+
+    }
     render() {
+        const {data} = this.state;
         return (
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+                <Cards data={data} />
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }
