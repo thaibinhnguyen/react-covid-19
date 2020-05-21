ecd3086b7ac42500214ca3ad26571b6497232afd
thaibinhnguyen <thaibinhnguyen26041996@gmail.com>
2020-05-21 22:05:38 +0200

    chart

--- src/api/index.js
+++ src/api/index.js
@@ -15,11 +15,16 @@ export const fetchData = async () => {
     }
 }
 
-export const fetchDailyDate = async () => {
+export const fetchDailyData = async () => {
     try {
         const {data} = await axios.get(`${url}/daily`);
-
-        return data;
+        // console.log(data)
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }))
+        return modifiedData;
     } catch (error) {
         
     }
